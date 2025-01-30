6eb52f61b26492fe7bea1866fdca4a8dc0d1796e
Donnerbart <community+github@donnerbart.de>
2025-01-30 03:39:29 +0000

    Add tests for argument validation

--- src/main/java/de/donnerbart/split/TestSplitMain.java
+++ src/main/java/de/donnerbart/split/TestSplitMain.java
@@ -4,11 +4,14 @@ import ch.qos.logback.classic.Level;
 import com.beust.jcommander.JCommander;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.VisibleForTesting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class TestSplitMain {
 
@@ -20,26 +23,17 @@ public class TestSplitMain {
             jCommander.usage();
             System.exit(0);
         }
-        if (arguments.splitTotal < 1) {
-            System.out.println("--split-total must be greater than 0");
-            System.exit(1);
-        }
-        if (arguments.splitIndex > arguments.splitTotal - 1) {
-            System.out.println("--split-index must lesser than --split-total");
-            System.exit(1);
-        }
-        final var workingDirectory = arguments.workingDirectory != null ?
-                arguments.workingDirectory.toAbsolutePath() :
-                Paths.get(System.getProperty("user.dir")).toAbsolutePath();
-        if (!Files.exists(workingDirectory)) {
-            System.out.println("Working directory does not exist: " + workingDirectory);
-            System.exit(1);
-        }
         if (arguments.debug) {
             final var root = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
             root.setLevel(Level.DEBUG);
         }
-
+        final var workingDirectory =
+                Objects.requireNonNullElse(arguments.workingDirectory, Paths.get(System.getProperty("user.dir")))
+                        .toAbsolutePath()
+                        .normalize();
+        if (!validate(arguments, workingDirectory)) {
+            System.exit(1);
+        }
         final var testSplit = new TestSplit(arguments.splitIndex,
                 arguments.splitTotal,
                 arguments.glob,
@@ -49,5 +43,22 @@ public class TestSplitMain {
                 arguments.debug,
                 System::exit);
         testSplit.run();
+    }
+
+    @VisibleForTesting
+    static boolean validate(final @NotNull Arguments arguments, final @NotNull Path workingDirectory) {
+        if (arguments.splitTotal < 1) {
+            System.out.println("--split-total must be greater than 0");
+            return false;
+        }
+        if (arguments.splitIndex > arguments.splitTotal - 1) {
+            System.out.println("--split-index must lesser than --split-total");
+            return false;
+        }
+        if (!Files.exists(workingDirectory)) {
+            System.out.println("Working directory does not exist: " + arguments.workingDirectory);
+            return false;
+        }
+        return true;
     }
 }
---
+++ src/test/java/de/donnerbart/split/TestSplitMainTest.java
@@ -0,0 +1,49 @@
+package de.donnerbart.split;
+
+import com.beust.jcommander.JCommander;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TestSplitMainTest {
+
+    private final @NotNull Arguments arguments = new Arguments();
+    private final @NotNull JCommander jCommander = JCommander.newBuilder().addObject(arguments).build();
+
+    @TempDir
+    private @NotNull Path tmp;
+
+    @Test
+    void validateArguments() {
+        jCommander.parse("-g", "**/*Test.java", "-t", "1", "-i", "0");
+        assertThat(TestSplitMain.validate(arguments, tmp)).isTrue();
+    }
+
+    @Test
+    void validateArguments_withZeroSplitTotal() {
+        jCommander.parse("-g", "**/*Test.java", "-t", "0", "-i", "0");
+        assertThat(TestSplitMain.validate(arguments, tmp)).isFalse();
+    }
+
+    @Test
+    void validateArguments_withNegativeSplitTotal() {
+        jCommander.parse("-g", "**/*Test.java", "-t", "-1", "-i", "0");
+        assertThat(TestSplitMain.validate(arguments, tmp)).isFalse();
+    }
+
+    @Test
+    void validateArguments_withTooSmallSplitIndex() {
+        jCommander.parse("-g", "**/*Test.java", "-t", "1", "-i", "1");
+        assertThat(TestSplitMain.validate(arguments, tmp)).isFalse();
+    }
+
+    @Test
+    void validateArguments_withInvalidWorkingDirectory() {
+        jCommander.parse("-g", "**/*Test.java", "-t", "1", "-i", "0");
+        assertThat(TestSplitMain.validate(arguments, tmp.resolve("does-not-exist"))).isFalse();
+    }
+}
