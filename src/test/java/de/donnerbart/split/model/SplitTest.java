506c3850390a7a42eee88c9fbd1bbe2a2f8dd8b0
David Sondermann <david.sondermann@hivemq.com>
2026-02-28 03:40:27 +0100

    Refactor equals and hashCode tests for Split and TestCase classes

--- src/test/java/de/donnerbart/split/model/SplitTest.java
+++ src/test/java/de/donnerbart/split/model/SplitTest.java
@@ -3,13 +3,10 @@ package de.donnerbart.split.model;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
 
-import static nl.jqno.equalsverifier.Warning.ALL_FIELDS_SHOULD_BE_USED;
-import static nl.jqno.equalsverifier.Warning.STRICT_INHERITANCE;
-
 class SplitTest {
 
     @Test
     void test_equalsAndHashCode() {
-        EqualsVerifier.forClass(Split.class).suppress(STRICT_INHERITANCE).suppress(ALL_FIELDS_SHOULD_BE_USED).verify();
+        EqualsVerifier.forClass(Split.class).withOnlyTheseFields("index").verify();
     }
 }
